1944cf06e75346d0d46e093174641f50eede7717
[Sajid Hasan] <[sajidhasanapp@gmail.com]>
2023-07-29 08:55:15 +0600

    DAy-7

--- lib/login.dart
+++ lib/login.dart
@@ -10,76 +10,102 @@ class login extends StatefulWidget {
 }
 
 class _loginState extends State<login> {
+  final _formKey = GlobalKey<FormState>();
   String name = "";
   bool changeButton = false;
+  moveToHome(BuildContext context) async {
+    if (_formKey.currentState!.validate()) {
+      setState(() {
+        changeButton = true;
+      });
+      await Future.delayed(Duration(seconds: 1));
+      Navigator.pushNamed(context, MyRoutes.HomeRoute);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Login Page")),
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Image.asset("assets/images/login.png", fit: BoxFit.cover),
-            SizedBox(
-              height: 20,
-            ),
-            Text(
-              "Welcome ${name}",
-              textScaleFactor: 1.5,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-              child: Column(
-                children: [
-                  TextField(
-                    decoration: InputDecoration(labelText: "Username"),
-                    onChanged: (value) {
-                      name = value;
-                      setState(() {});
-                    },
-                  ),
-                  TextField(
-                    obscureText: true,
-                    decoration: InputDecoration(labelText: "Password"),
-                  ),
-                  SizedBox(
-                    height: 15,
-                  ),
-                  InkWell(
-                    onTap: () async {
-                      setState(() {
-                        changeButton = true;
-                      });
-                      await Future.delayed(Duration(seconds: 1));
-                      Navigator.pushNamed(context, MyRoutes.HomeRoute);
-                    },
-                    child: AnimatedContainer(
-                      duration: Duration(seconds: 1),
-                      width: changeButton ? 40 : 150,
-                      height: 40,
-                      alignment: Alignment.center,
-                      decoration: BoxDecoration(
-                          color: Colors.deepPurple,
-                          borderRadius:
-                              BorderRadius.circular(changeButton ? 50 : 8)),
-                      child: changeButton
-                          ? Icon(
-                              Icons.done,
-                              color: Colors.white,
-                            )
-                          : Text(
-                              "Login",
-                              style: TextStyle(
-                                color: Colors.white,
-                              ),
-                            ),
-                    ),
-                  )
-                ],
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              Image.asset("assets/images/login.png", fit: BoxFit.cover),
+              SizedBox(
+                height: 20,
               ),
-            )
-          ],
+              Text(
+                "Welcome ${name}",
+                textScaleFactor: 1.5,
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(labelText: "Username"),
+                      onChanged: (value) {
+                        name = value;
+                        setState(() {});
+                      },
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Username cannot be empty";
+                        }
+                        return null;
+                      },
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(labelText: "Password"),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Password cannot be empty";
+                        } else if (value.length < 6) {
+                          return "Password length should be atleast 6";
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Material(
+                      borderRadius:
+                          BorderRadius.circular(changeButton ? 50 : 8),
+                      color: Colors.deepPurple,
+                      child: InkWell(
+                        splashColor: Colors.amber,
+                        onTap: () {
+                          moveToHome(context);
+                        },
+                        child: AnimatedContainer(
+                          duration: Duration(seconds: 1),
+                          width: changeButton ? 40 : 150,
+                          height: 40,
+                          alignment: Alignment.center,
+                          child: changeButton
+                              ? Icon(
+                                  Icons.done,
+                                  color: Colors.white,
+                                )
+                              : Text(
+                                  "Login",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                  ),
+                                ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
       drawer: Drawer(),
